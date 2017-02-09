c956830ed2b8f74439d43eba0ceba112d48f0e32
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-02-09 18:01:36 +0100

    scripts definitivos testeados

--- jhipster_latest.sh
+++ jhipster_latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "pulling jhipster image"
-docker pull jhipster/jhipster
+docker pull jhipster/jhipster:latest
 mkdir ~/jhipster
 docker run --name jhipster -v ~/jhipster:/home/jhipster/app -v ~/.m2:/home/jhipster/.m2 -p 8080:8080 -p 3000:3000 -p 3001:3001 -d -t jhipster/jhipster
 echo "estado del container docker"
