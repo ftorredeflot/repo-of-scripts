b02fde781986e4741a13d66043a790279886fb99
Ferran Torredeflot <ftorredeflot@gmail.com>
2018-04-15 20:19:54 +0200

    list

--- myfirtpython.py
+++ myfirtpython.py
@@ -7,3 +7,16 @@ myfloat = 7.0
 print(myfloat)
 myfloat = float(7)
 print(myfloat)
+
+
+mylist = [1, 2, 3]
+
+mylist.append("hola")
+mylist.append(5)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+print(mylist[3])
+# prints out 1,2,3
+for x in mylist:
+    print(x)
