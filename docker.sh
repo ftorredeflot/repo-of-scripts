a387dfb45b87715c8d99411b79368b5619c3e146
ftorredeflot <ftorredeflot@gmail.com>
2018-06-08 11:50:29 +0200

    redirect outputs

--- docker.sh
+++ docker.sh
@@ -5,14 +5,14 @@ apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common > /dev/null 2> /dev/null
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
-apt update
+   stable" > /dev/null 2> /dev/null
+apt update > /dev/null 2> /dev/null
 apt install -y docker-ce
 usermod -aG docker $name
 docker version
