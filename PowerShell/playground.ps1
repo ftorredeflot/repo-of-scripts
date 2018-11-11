50fe5eef06dbe06595afd605d009e775d112c545
ftorredeflot <ftorredeflot@gmail.com>
2018-11-11 20:46:42 +0100

    new file in powershell

---
+++ PowerShell/playground.ps1
@@ -0,0 +1,17 @@
+﻿$PSVersionTable.PSVersion.Major
+
+Get-Help Add-BitsFile -ShowWindow
+
+
+Get-Command *Archive*
+
+get-clipboard
+
+Set-Clipboard -Value "que loco" 
+
+Write-Host (1..32)
+
+$apw= Read-Host -AsSecureString
+$cred= new-object -TypeName PSCredential -ArgumentList("paco",($apw))
+echo $cred
+$cred.GetNetworkCredential().Password
