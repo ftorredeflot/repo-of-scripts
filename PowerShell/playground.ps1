e93efb051a51d6759c259537ccff1a7a4303bca2
ftorredeflot <ftorredeflot@gmail.com>
2019-11-09 17:37:32 +0100

    getting and setting  registry keys

--- PowerShell/playground.ps1
+++ PowerShell/playground.ps1
@@ -1,17 +1,41 @@
-﻿$PSVersionTable.PSVersion.Major
+﻿#$PSVersionTable.PSVersion.Major
 
-Get-Help Add-BitsFile -ShowWindow
+#Get-Help Add-BitsFile -ShowWindow#
 
 
-Get-Command *Archive*
+#Get-Command *Archive*#
 
-get-clipboard
 
-Set-Clipboard -Value "que loco" 
+#get-clipboard#
 
-Write-Host (1..32)
+#Set-Clipboard -Value "que loco"
+#Get-Clipboard
+#Write-Host (1..32)
 
-$apw= Read-Host -AsSecureString
+<# $apw= Read-Host -AsSecureString
 $cred= new-object -TypeName PSCredential -ArgumentList("paco",($apw))
 echo $cred
-$cred.GetNetworkCredential().Password
+$cred.GetNetworkCredential().Password #>
+
+#Get-Command -FullyQualifiedModule DataOntap| Select -First 2
+#Get-Command -FullyQualifiedModule PackageManagement#
+
+#Get-Command -FullyQualifiedModule PowerShellGet #
+
+#$pstest= Get-process | select -first 7
+
+#$pstest|ft
+
+# Get-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" | Select-Object -ExpandProperty Property
+# Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" | fl Productname, ReleaseId
+#Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" | fl Productname, ReleaseId
+Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name  Productname, ReleaseId
+$PSVersionTable.PSVersion
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine" -Name powershellversion
+#set location
+Set-Location "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+Get-ItemPropertyValue -Path . -Name productname
+Set-Location -Path "C:\"
+Set-Location "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+
+#create registry key
