8211fb66206f511dd419850be38b85dc90e823ce
ftorredeflot <ftorredeflot@gmail.com>
2019-11-10 11:33:00 +0100

    added error handling

---
+++ PowerShell/errorhandling.ps1
@@ -0,0 +1,13 @@
+﻿#error handling
+
+$Error.Count
+#throw eror
+#get-thisdoesnotexist
+#view errors
+$Error.Count
+#view error datatype
+$Error.GetType().name
+#show erroe exception message
+$error[0].Exception
+#show exception type
+$error[0].Exception.GetType().name
