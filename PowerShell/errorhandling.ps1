b0ffd813f83abe97c710df1609c99b649db2c764
ftorredeflot <ftorredeflot@gmail.com>
2019-11-10 14:50:56 +0100

    configuring the error response

--- PowerShell/errorhandling.ps1
+++ PowerShell/errorhandling.ps1
@@ -11,3 +11,19 @@ $Error.GetType().name
 $error[0].Exception
 #show exception type
 $error[0].Exception.GetType().name
+
+New-Object -PropertyDoesNotExist
+$Error.Count
+$Error[0].Exception.GetType().name
+
+#configure error action
+$ErrorActionPreference
+$ErrorActionPreference="Stop"
+$ErrorActionPreference="SilentlyContinue"
+
+#error action parameter
+get-childitem "h:\doesnotexist" -ErrorAction Ignore
+$Error.Count
+
+get-childitem "h:\doesnotexist" -ErrorAction inquire
+get-childitem "h:\doesnotexist" -ea SilentlyContinue
