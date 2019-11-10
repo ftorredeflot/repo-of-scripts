101be76aa28fd9b4b33bb65724e5e06960c92d40
ftorredeflot <ftorredeflot@gmail.com>
2019-11-10 18:07:00 +0100

    manage event-logs

---
+++ PowerShell/eventlog.ps1
@@ -0,0 +1,22 @@
+﻿#needs admin rights
+
+#new-eventlog
+
+$strLogName="New Test Event"
+$strLogSource=$strLogName
+New-EventLog -LogName $strLogName -Source $strLogSource
+
+#write-eventlog
+
+for($i=0;$i -lt 7; $i++)
+{
+if ($i%2){$strType="Error"}ELSE{$strType="Warning"}
+$strMessage="Creating $strType event"
+Write-EventLog -LogName $strLogName -Source $strLogName -Message $strMessage -EventId $i -EntryType $strType
+}
+
+Get-EventLog -List |?{$_.Log -eq $strLogName}
+Get-EventLog -LogName Application -Newest 8 |ft Source,InstanceId,EntryType,Message -AutoSize
+
+#openeventviewer 
+Show-EventLog
