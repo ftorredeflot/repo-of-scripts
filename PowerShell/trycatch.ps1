6febaa6beb2bcc67cd36247e29a8394ea783be67
ftorredeflot <ftorredeflot@gmail.com>
2019-11-10 14:56:41 +0100

    try catch divide per zero

--- PowerShell/trycatch.ps1
+++ PowerShell/trycatch.ps1
@@ -1,6 +1,27 @@
-﻿try
+﻿#try
+#{
+#New-Object $objnew
+#}
+#catch
+#{
+#Write-Host "caught a system exception"
+#}
+#finally{
+#Write-Host "clean up"
+#}
+
+
+#multiple catch
+#only catch the first error
+
+try
 {
+1/0
 New-Object $objnew
+}
+catch [DivideByZeroException]{
+
+Write-Host "caught a divide by zero exception"
 }
 catch
 {
